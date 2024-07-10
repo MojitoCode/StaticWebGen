f73e6446db015a4f0bccb235003d6b7b51d66b2a
zomb37 <howlingnights@gmail.com>
2024-07-09 20:43:58 -0700

    daily backup

--- src/parentnode.py
+++ src/parentnode.py
@@ -1,5 +1,14 @@
 from htmlnode import HTMLNode
 from leafnode import LeafNode
 
-class ParentNode:
-	
+class ParentNode(HTMLNode):
+	def __init__(self, tag, children):
+		super.__init__(tag=tag, value=None)
+
+		if not children:
+			raise valueError("Children must have a value.")
+		self.children = children
+
+	def to_html():
+		if not tag:
+			raise valueError("Value is required")
