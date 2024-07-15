f4371dd4294ce7d867e6f83f07ba616b932532d3
zomb37 <howlingnights@gmail.com>
2024-07-14 19:23:57 -0700

    added a new file for the 'textnode to htmlnode' function'; added textnode constants to the textnode.py file

--- src/textnode.py
+++ src/textnode.py
@@ -13,3 +13,10 @@ class TextNode:
 
 	def __repr__(self):
 		return f"TextNode({self.text}, {self.text_type}, {self.url})"
+#define textnode types - constants
+TEXT_TYPE_TEXT = "text"
+TEXT_TYPE_BOLD = "bold"
+TEXT_TYPE_ITALIC = "italic"
+TEXT_TYPE_CODE = "code"
+TEXT_TYPE_LINK = "link"
+TEXT_TYPE_IMAGE = "image"
