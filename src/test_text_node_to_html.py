a803c294b076b4468f1c10735e6a60a02ef240b6
zomb37 <howlingnights@gmail.com>
2024-07-19 23:59:14 -0700

    finished nodes

--- src/test_text_node_to_html.py
+++ src/test_text_node_to_html.py
@@ -10,32 +10,32 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         text_node = TextNode(text="Sample text", text_type=TEXT_TYPE_TEXT)
         html_node = text_node_to_html_node(text_node)
         self.assertEqual(html_node.tag, '')
-        self.assertEqual(html_node.text, "Sample text")
+        self.assertEqual(html_node.value, "Sample text")
     
     def test_text_type_bold(self):
         text_node = TextNode(text="Bold text", text_type=TEXT_TYPE_BOLD)
         html_node = text_node_to_html_node(text_node)
         self.assertEqual(html_node.tag, 'b')
-        self.assertEqual(html_node.text, "Bold text")
+        self.assertEqual(html_node.value, "Bold text")
         
     def test_text_type_italic(self):
         text_node = TextNode(text="Italic text", text_type=TEXT_TYPE_ITALIC)
         html_node = text_node_to_html_node(text_node)
         self.assertEqual(html_node.tag, 'i')
-        self.assertEqual(html_node.text, "Italic text")
+        self.assertEqual(html_node.value, "Italic text")
     
     def test_text_type_code(self):
         text_node = TextNode(text="Code text", text_type=TEXT_TYPE_CODE)
         html_node = text_node_to_html_node(text_node)
         self.assertEqual(html_node.tag, 'code')
-        self.assertEqual(html_node.text, "Code text")
+        self.assertEqual(html_node.value, "Code text")
         
     def test_text_type_link(self):
         text_node = TextNode(text="Link text", text_type=TEXT_TYPE_LINK, href="https://example.com")
         html_node = text_node_to_html_node(text_node)
         self.assertEqual(html_node.tag, 'a')
-        self.assertEqual(html_node.text, "Link text")
-        self.assertEqual(html_node.href, "https://example.com")
+        self.assertEqual(html_node.value, "Link text")
+        self.assertEqual(html_node.props.get("href"), "https://example.com")
         
     def test_text_type_image(self):
         text_node = TextNode(text="", text_type=TEXT_TYPE_IMAGE, src="https://example.com", alt="some test text")
--- src/text_node_to_html.py
+++ src/text_node_to_html.py
@@ -1,22 +1,26 @@
+# text_node_to_html.py
+
 from textnode import TextNode, TEXT_TYPE_TEXT, TEXT_TYPE_BOLD, TEXT_TYPE_ITALIC, TEXT_TYPE_CODE, TEXT_TYPE_LINK, TEXT_TYPE_IMAGE
 from leafnode import LeafNode
 from htmlnode import HTMLNode
 
 def text_node_to_html_node(text_node):
-	if not isinstance(text_node, TextNode):
-		raise ValueError("Input value must be a TextNode")
-	
-	if text_node.text_type == TEXT_TYPE_TEXT:
-		return LeafNode(tag='', text=text_node.text)
-	elif text_node.text_type == TEXT_TYPE_BOLD:
-                return LeafNode(tag='b', text=text_node.text)
-	elif text_node.text_type == TEXT_TYPE_ITALIC:
-                return LeafNode(tag='i', text=text_node.text)
-	elif text_node.text_type == TEXT_TYPE_CODE:
-                return LeafNode(tag='code', text=text_node.text)
-	elif text_node.text_type == TEXT_TYPE_LINK:
-                return LeafNode(tag='a', text=text_node.text, href=text_node.href)
-	elif text_node.text_type == TEXT_TYPE_IMAGE:
-                return LeafNode(tag='img', text='', src=text_node.src, alt=text_node.alt)
-	else:
-		raise ValueError(f"Unsupported TextNode type: {text_node.text_type}")
+    if not isinstance(text_node, TextNode):
+        raise ValueError("Input value must be a TextNode")
+
+    if text_node.text_type == TEXT_TYPE_TEXT:
+        return LeafNode(tag='', value=text_node.text)
+    elif text_node.text_type == TEXT_TYPE_BOLD:
+        return LeafNode(tag='b', value=text_node.text)
+    elif text_node.text_type == TEXT_TYPE_ITALIC:
+        return LeafNode(tag='i', value=text_node.text)
+    elif text_node.text_type == TEXT_TYPE_CODE:
+        return LeafNode(tag='code', value=text_node.text)
+    elif text_node.text_type == TEXT_TYPE_LINK:
+        props = {"href": text_node.href} if text_node.href else {}
+        return LeafNode(tag='a', value=text_node.text, props=props)
+    elif text_node.text_type == TEXT_TYPE_IMAGE:
+        props = {"src": text_node.src, "alt": text_node.alt} if text_node.src and text_node.alt else {}
+        return LeafNode(tag='img', value='', props=props)
+    else:
+        raise ValueError(f"Unsupported TextNode type: {text_node.text_type}")
